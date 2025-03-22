1bfaaae0927882dce7eb772dd8dd9d445e236612
Isi Name <perpetualokan5@gmail.com>
2025-03-22 13:54:33 +0000

    Updated Rooms.jsx

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./Rooms.css";
 
 const roomsData = [
-  { id: 1, name: "Classic Room", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742560040/classic_vmrxsn.mp4", description: "A cozy room with all basic amenities." },
-  { id: 2, name: "Super Classic Room", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742559633/standard_ufzylf.mp4", description: "An upgraded classic room with more space." },
-  { id: 3, name: "Standard Deluxe", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742560977/stan1_jm2xec.mp4", description: "A luxurious room with deluxe features." },
-  { id: 4, name: "Executive Deluxe", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742561330/8849242-hd_1920_1080_24fps_n9mss0.mp4", description: "A premium room for executives." },
+  { id: 1, name: "Classic Room", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742647607/19403230-hd_1920_1080_25fps_fc1yso.mp4", description: "A cozy room with all basic amenities." },
+  { id: 2, name: "Super Classic Room", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742647217/9663211-hd_1920_1080_30fps_u6pxlq.mp4", description: "An upgraded classic room with more space." },
+  { id: 3, name: "Standard Deluxe", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742648005/3773489-hd_1920_1080_30fps_h4be2h.mp4", description: "A luxurious room with deluxe features." },
+  { id: 4, name: "Executive Deluxe", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742645409/executive_11_xm5cge.mp4", description: "A premium room for executives." },
   { id: 5, name: "Business Suite", video: "https://res.cloudinary.com/dlxbur3on/video/upload/v1742559624/business_suite_jzb4qm.mp4", description: "A spacious suite designed for business travelers." }
 ];
 
